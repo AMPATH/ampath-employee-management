65c7fa4443f6cfeed6008d908673809d54aeb885
Maureen Nduta <67265839+maureennduta@users.noreply.github.com>
2021-09-21 12:41:34 +0300

    Linting Updates

--- front-end/src/tests/login.test.tsx
+++ front-end/src/tests/login.test.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Login from "../Components/Login/login";
+import React from 'react';
+import { render } from '@testing-library/react';
+import Login from '../Components/Login/login';
 
-describe("Login render Page", () => {
-  it("renders the input fields and login button", () => {
+describe('Login render Page', () => {
+  it('renders the input fields and login button', () => {
     const { getByTestId } = render(<Login />);
-    expect(getByTestId("username")).toBeInTheDocument();
-    expect(getByTestId("password")).toBeInTheDocument();
-    expect(getByTestId("login")).toBeInTheDocument();
+    expect(getByTestId('username')).toBeInTheDocument();
+    expect(getByTestId('password')).toBeInTheDocument();
+    expect(getByTestId('login')).toBeInTheDocument();
   });
 });
