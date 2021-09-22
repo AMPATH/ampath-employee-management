ca13d5d17c0077e01cc08bec792e8fd63e4f314a
walter <walterkiprono81@gmail.com>
2021-09-22 10:59:02 +0300

    resolve conflict: package.json and package-lock.json files

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import { EmployeeRegistrationForm } from './Employee/Registration/employee-registration.component';
 
 function App() {
   return (
     <>
-      <EmployeeRegistrationForm />
+      <h3>Modules under development</h3>
+      <h5>Check them on ./Components folder</h5>
     </>
   );
 }
