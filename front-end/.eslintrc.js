5ee07079fcd01927dfa8c4f70deb06ff7af6ee66
walter <walterkiprono81@gmail.com>
2021-10-01 12:09:04 +0300

    refactor: removed front-end folder

--- front-end/.eslintrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: ['plugin:prettier/recommended'],
-  plugins: ['@typescript-eslint'],
-};
--- front-end/src/Components/Employee/Profile/Resource.tsx
+++
@@ -1,2 +0,0 @@
-const BaseUrl = 'http://10.50.80.56:5100/';
-export default BaseUrl;
