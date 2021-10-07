f324d9738e8fd97e259d62dc51dc3c502856b92a
maureennduta <mnduta57@gmail.com>
2021-10-07 17:43:57 +0300

    Changes made to login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, useHistory } from 'react-router-dom';
 import './App.css';
 import NavigationBar from './Components/Navigation/NavigationBar';
+import { Login } from './Components/Login/login';
+import ProtectedRoutes from './Components/ProtectedRoutes/ProtectedRoutes';
+
+const token = localStorage.getItem('token');
+
 function App() {
+  const [isLoggedIn] = useState(localStorage.getItem('token'));
+  const history = useHistory();
+  useEffect(() => {
+    {
+      token;
+    }
+  }, []);
   return (
     <BrowserRouter>
-      <div className="App">
-        <NavigationBar />
-      </div>
+      {token ? (
+        <div className="App">
+          <NavigationBar />
+        </div>
+      ) : (
+        <Route path="/">
+          <Login />
+        </Route>
+      )}
     </BrowserRouter>
   );
 }
