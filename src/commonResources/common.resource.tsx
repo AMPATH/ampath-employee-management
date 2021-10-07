3f6466fbf15605cb6f123c6fc059a37fe1b0ff84
Cynthia Chebet <40205991+KorirC@users.noreply.github.com>
2021-10-07 11:36:18 +0300

    Update common.resource.tsx

--- src/commonResources/common.resource.tsx
+++ src/commonResources/common.resource.tsx
@@ -12,32 +12,32 @@ export const getDepartments = async () => {
   });
 };
 export const getProjects = async () => {
-  return useFetch(`${url}/project`).then((response) => {
+  return await useFetch(`${url}/project`).then((response) => {
     return response.data;
   });
 };
 export const getPrograms = async () => {
-  return useFetch(`${url}/program`).then((response) => {
+  return await useFetch(`${url}/program`).then((response) => {
     return response.data;
   });
 };
 export const getSites = async () => {
-  return useFetch(`${url}/site`).then((response) => {
+  return await useFetch(`${url}/site`).then((response) => {
     return response.data;
   });
 };
 export const getBudgets = async () => {
-  return useFetch(`${url}/budget`).then((response) => {
+  return await useFetch(`${url}/budget`).then((response) => {
     return response.data;
   });
 };
 export const trackEmployees = async () => {
-  return useFetch(`${url}/movement`).then((response) => {
+  return await useFetch(`${url}/movement`).then((response) => {
     return response.data;
   });
 };
 export const getReport = async (param: any) => {
-  return useFetch(
+  return await useFetch(
     `${url}/report?department=${param.department}&project=${param.project}&site=${param.site}&budget=${param.budget}&county=${param.county}&status=${param.contractStatus}&program=${param.programArea}`,
   ).then((response) => {
     return response.data;
