5a8ab070ddeccd4c929148efc71442a0a415d064
maureennduta <mnduta57@gmail.com>
2021-10-19 14:12:06 +0300

    Login and Register Test Update

--- src/tests/login.test.tsx
+++ src/tests/login.test.tsx
@@ -1,6 +1,35 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import { Login } from '../Components/Login/login';
+import renderer from 'react-test-renderer';
+import ReactDOM from 'react-dom';
+
+//login renders without crashing
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Login setIsAuthenticated={undefined} />, div);
+});
+
+it('submits correct values', async () => {
+  const { container } = render(<Login setIsAuthenticated={undefined} />);
+  const username = container.querySelector('input[name="userName"]')!;
+  const password = container.querySelector('input[name="password"]')!;
+  const submit = container.querySelector('button[type="submit"]');
+
+  await waitFor(() => {
+    fireEvent.change(username, {
+      target: {
+        value: 'mockname',
+      },
+    });
+
+    fireEvent.change(password, {
+      target: {
+        value: 'mockpassword',
+      },
+    });
+  });
+});
 
 describe('Login render Page', () => {
   it('renders the input fields and login button', () => {
